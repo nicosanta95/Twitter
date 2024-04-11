6dbe3c11ca99953dfdd001427eec4cd3459e85fb
Roberto <roberto.iaconelli1@gmail.com>
2024-04-11 21:49:37 +0200

    First

---
+++ js/script01.js
@@ -0,0 +1,68 @@
+let nickname = document.querySelector("#nickname");
+let password = document.querySelector("#password");
+let btnLogin = document.querySelector("#bntRegistrati");
+let logoX = "./img/logouccellino.jpg";
+
+
+function controlloUsername() {
+  const regex = /^.{4,15}$/; // Regex che corrisponde a stringhe di lunghezza tra 4 e 15 caratteri inclusi
+  nickname = document.getElementById("nickname"); // Ottieni l'elemento input del nickname
+
+  if (nickname.value.match(regex)) {
+        return true; // Salva il nickname nel localStorage se è valido
+  } else {
+
+        erroriUtente.innerHTML += "Inserire dai 4 ai 15 caratteri."
+       
+
+
+  }
+}
+
+function controlloPassword() {
+  const regexPassword =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/;
+  password = document.getElementById("password");
+  if (password.value.match(regexPassword)) {
+        return true;
+  } else {
+        erroriPassword.innerHTML += "Inserire almeno 1 lettera minuscola,<br> 1 maiuscola,<br> 1 numero,<br> 1 carattere speciale <br> Minimo 8 caratteri."
+       
+        
+  }
+}
+
+function registraUtente(){
+    let utenteOk = controlloUsername();
+    let passwordOk = controlloPassword();
+    if(utenteOk && passwordOk){
+        // console.log(nickname, password);
+        let nuovoUtente = new Utente(nickname.value, password.value,[]);
+        localStorage.setItem("nuovoUtente", JSON.stringify(nuovoUtente));
+              
+       
+    }else{
+        console.log("Errore");
+    }
+}
+
+let nicknameUtente = localStorage.getItem("nickname");
+
+
+
+class Utente {
+  constructor(nome, password, tweets){
+      this.nome=nome;
+      this.password=password;
+      this.tweets=tweets;
+
+      }
+
+  }
+
+  
+
+
+
+btnLogin.addEventListener("click", registraUtente);
+
---
+++ js/script02.js
@@ -0,0 +1,61 @@
+
+let textArea = document.querySelector("#textArea");
+let caratteriRimanenti = document.querySelector("#conteggio")
+let erroriUtente = document.querySelector("#erroriUtente");
+let erroriPassword = document.querySelector("#erroriPassword");
+let post = document.querySelector("#post");
+let btnTweet = document.querySelector("#bntTweet");
+
+function scalaCaratteri(){
+    let caratteriRimanenti = textArea.maxLength - textArea.value.length;
+    conteggio.textContent = caratteriRimanenti;
+  }
+
+  let utenteLoggato = JSON.parse(localStorage.getItem("nuovoUtente"));
+  console.log(utenteLoggato);
+
+
+    class Tweet {
+        constructor(contenuto,data){
+            this.contenuto=contenuto;
+            this.data=data;
+
+        }
+    }
+
+    function aggiungiTweet(){
+        let data= new Date();
+        data = data.getTime();
+        let nuovoTweet = new Tweet(textArea.value, data.value);
+        [...utenteLoggato].push(nuovoTweet);
+        console.log(utenteLoggato);
+    }
+    
+    
+    
+
+
+
+
+
+
+//  function stampaPost(event){
+//    event.preventDefault();
+//    post.innerHTML="";
+   
+   
+//    if(tweet.trim() == ""){
+//      post.innerHTML += "Il campo è vuoto, tweet non postabile"
+//      textArea.value = "";
+//     }else{
+//       array.push(tweet);
+//       post.innerHTML += array;
+//       console.log(array);
+//       textArea.value = "";
+//       localStorage.setItem("tweet", array);
+//   }
+// }
+
+textArea.addEventListener("input", scalaCaratteri);
+ btnTweet.addEventListener("click", aggiungiTweet);
+// document.addEventListener('DOMContentLoaded', stampaPost);
